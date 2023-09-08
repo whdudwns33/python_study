76453be53bbfe9c0f797172371a0bc3c2e577c42
jojo6807 <jyj05181@gmail.com>
2023-09-08 17:35:49 +0900

    문제3

--- 문제3.py
+++ 문제3.py
@@ -1,38 +1,39 @@
 #리스트 연습문제.
 #1. 상근날드(초급)800 700 900 198 330
 
-arr = list(map(int, input().split()))
-hb = min(arr[:3])
-dr = min(arr[3:])
-print(hb)
-print(dr)
-print(dr + hb - 50)
+# arr = list(map(int, input().split()))
+# hb = min(arr[:3])
+# dr = min(arr[3:])
+# print(hb)
+# print(dr)
+# print(dr + hb - 50)
 
 #2. 저항문제(중급)
 # 저항색: black, brown, red, orange, yello, green, blue, violet, grey, white
 
-rg = ["black", "brown", "red", "orange", "yello", "green", "blue", "violet", "grey", "white"]
-n = [0,1,2,3,4,5,6,7,8,9]
-
-c1, c2 , c3 = input("색입력").split()
-for i in range (len(rg)):
-    if c1 == rg[i]:
-        a = n[i]*10
-
-for i in range (len(rg)):
-    if c2 == rg[i]:
-        b = n[i]
-
-for i in range (len(rg)):
-    if c2 == rg[i]:
-        c = 10**(i)
-print((a+b)*c)
+# rg = ["black", "brown", "red", "orange", "yello", "green", "blue", "violet", "grey", "white"]
+# n = [0,1,2,3,4,5,6,7,8,9]
+#
+# c1, c2 , c3 = input("색입력").split()
+# for i in range (len(rg)):
+#     if c1 == rg[i]:
+#         a = n[i]*10
+#
+# for i in range (len(rg)):
+#     if c2 == rg[i]:
+#         b = n[i]
+#
+# for i in range (len(rg)):
+#     if c2 == rg[i]:
+#         c = 10**(i)
+# print((a+b)*c)
 
 #3. pc방 알바(중급)
 
 seat = []
 cnt = 0
 h = int(input("손님 수:"))
+print("종료 코드 999")
 while True:
     num = int(input("좌석입력:"))
     if num < 100 and num not in seat :
@@ -40,19 +41,23 @@ while True:
     elif num in seat :
         print("거절")
         cnt += 1
-    else:
+    elif num > 100:
         print("다시 입력")
-    if num >100:
-        print(sorted(seat))
-        print(f"거절횟수: {cnt}")
-        break
+    else :
+        if num == 999 :
+            print(sorted(seat))
+            print(f"거절횟수: {cnt}")
+            break
+
+
 
 #4. KMP 암호 문제
-str = list(input().split("-"))
-val = " "
-for i in range(0,len(str),1):
-    val = str[i][0].upper()
-    print(val,end="")
+
+# str = list(input().split("-"))
+# val = " "
+# for i in range(0,len(str),1):
+#     val = str[i][0].upper()
+#     print(val,end="")
 
 
 
