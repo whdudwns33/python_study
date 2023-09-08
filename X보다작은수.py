4da8c056172d61cd2a481cf9d5a5b20d04ecaa06
jojo6807 <jyj051811@gmail.com>
2023-09-08 19:09:53 +0900

    X보다 작은 수

--- X보다작은수.py
+++ X보다작은수.py
@@ -1,3 +1,4 @@
+# 1.
 A = list(map(int, input().split()))
 n = int(input())
 arr =[]
@@ -5,4 +6,10 @@ for i in range(len(A)):
     if A[i] < n :
         arr.append(A[i])
 print(arr)
+# 2.
+n, x = int(input().split())
+A = list(map(int, input().split()))
 
+for i in range(n):
+    if A[i] < x :
+        print(A[i], end= " ")
