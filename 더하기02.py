ff2c66e1830acccbbbd7b574cb08a0281dcf4776
jojo6807 <jyj05181@gmail.com>
2023-09-08 13:57:30 +0900

    더하기02

--- 더하기02.py
+++ 더하기02.py
@@ -1,4 +1,5 @@
 n = int(input())
 for i in range(n) :
-    arr = list(map(int, input().split()))
-    print(f"Case #{i+1}: {arr[1] + arr[2]}")
+    a, b = map(int, input().split())
+    print(f"Case #{i+1}: {a + b}")
+
