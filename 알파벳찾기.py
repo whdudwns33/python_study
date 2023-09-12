648e0c0b60b30ffd7a7bc783c7a4667d3b94634f
jojo6807 <jyj05181@gmail.com>
2023-09-12 18:27:42 +0900

    알파벳찾기

--- 알파벳찾기.py
+++ 알파벳찾기.py
@@ -2,10 +2,11 @@ alp = []
 rst = []
 for i in range(97, 124):
     alp.append(chr(i))
+
 s = input()
-for i in range(alp):
-    if s == alp:
-        rst.append(alp.index())
+for i in range(len(alp)):
+    if s[:] == alp[i]:
+        rst.append(alp[i])
     else:
         rst.append(-1)
-print(alp)
+print(rst)
