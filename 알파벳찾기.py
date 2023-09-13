f2698f073ecbc99921d79ad65cf8280897f1c701
jojo6807 <jyj05181@gmail.com>
2023-09-13 12:06:29 +0900

    알파벳찾기

--- 알파벳찾기.py
+++ 알파벳찾기.py
@@ -1,13 +1,21 @@
+# s의 i번째 열 글자가 포함되어 있으면 알파벳 순서의 j번째에 i가 들어가야함
+# ad
+# a => 0,
+# 0, -1, -1, +1
 alp = []
-rst = []
-for i in range(97, 124):
+ch = [] * 26
+rst = [-1]*26
+s = list(input())
+# ord() 문자열을 아스키 코드로 변환
+for i in range(97, 123):
     alp.append(chr(i))
+# print(ord(s[0]))
 
-s = input()
-for i in range(len(alp)):
-    for j in range(len(s)):
-        if s[:] == alp[i]:
-            rst.append(alp[i])
-        else:
-            rst.append(-1)
-print(rst)
+for i in range(len(s)):
+    if alp.:
+        rst[]
+
+
+
+
+
