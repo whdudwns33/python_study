cb47c721d08adced4fd7a4b2cd9d8ced4ba75391
jojo6807 <jyj05181@gmail.com>
2023-09-14 12:32:01 +0900

    상수

--- 상수.py
+++ 상수.py
@@ -1,8 +1,9 @@
-n1, n2 = map(str, input().split())
+n1, n2 = map(int, input().split())
 number1 = ''
 number2 = ''
-for e in n1:
+for e in str(n1):
     number1 = e + number1
+for e in str(n2):
     number2 = e + number2
 print(max(int(number1),int(number2)))
 
