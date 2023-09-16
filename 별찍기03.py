84192b7e43fe502e211d1113a206b17055910790
jojo6807 <jyj051811@gmail.com>
2023-09-16 22:49:26 +0900

    별찍기

--- 별찍기03.py
+++ 별찍기03.py
@@ -1,4 +1,15 @@
 n = int(input())
-for ()
+for i in range(n):
+    for a in range(n - i - 1):
+        print(" ", end="")
+    for b in range(2*i + 1):
+        print("*", end="")
+    print()
+for i in range(n):
+    for c in range(i + 1):
+        print(" ", end="")
+    for d in range(2*(n - i - 1) - 1):
+        print("*",end="")
+    print()
 
 
