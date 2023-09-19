0c856578d07d1175dbef18ea5911a46dcec746a6
jojo6807 <jyj051811@gmail.com>
2023-09-19 23:22:00 +0900

    그룹단어 체커 잘 못 풀고 있었음

--- 그룹단어체커.py
+++ 그룹단어체커.py
@@ -1,14 +1,23 @@
 n = int(input())
-cnt = 1;
-for i in range(n - 1):
+rst = []
+num = []
+for i in range(n):
     s = list(input())
     for j in range(len(s)):
         while True:
             if (j + 1) < len(s):
                 if ord(s[j]) < ord(s[j + 1]):
-                    cnt += 1
+                    rst.append(i)
+                    a = (len(rst)// (len(s)-1))
+                    if a == 1:
+                        num.append(i)
+                        break
+                    else:
+                        break
                 else:
                     break
             else:
                 break
-print(cnt)
+print(len(num))
+
+
