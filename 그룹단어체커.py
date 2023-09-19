8f1fa4af0b5f03b5e089269512fce5f1dbb93615
jojo6807 <jyj051811@gmail.com>
2023-09-19 23:44:12 +0900

    그룹단어 체커 잘 못 풀고 있었음

--- 그룹단어체커.py
+++ 그룹단어체커.py
@@ -1,23 +1,32 @@
+# n = int(input())
+# rst = []
+# num = []
+# for i in range(n):
+#     s = list(input())
+#     for j in range(len(s)):
+#         while True:
+#             if (j + 1) < len(s):
+#                 if ord(s[j]) < ord(s[j + 1]):
+#                     rst.append(i)
+#                     a = (len(rst)// (len(s)-1))
+#                     if a == 1:
+#                         num.append(i)
+#                         break
+#                     else:
+#                         break
+#                 else:
+#                     break
+#             else:
+#                 break
+# print(len(num))
+#
+# 잘 못 풀음
+
 n = int(input())
 rst = []
-num = []
 for i in range(n):
     s = list(input())
-    for j in range(len(s)):
-        while True:
-            if (j + 1) < len(s):
-                if ord(s[j]) < ord(s[j + 1]):
-                    rst.append(i)
-                    a = (len(rst)// (len(s)-1))
-                    if a == 1:
-                        num.append(i)
-                        break
-                    else:
-                        break
-                else:
-                    break
-            else:
-                break
-print(len(num))
+
+
 
 
