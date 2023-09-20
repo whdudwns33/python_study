4ff351951124f520d48760c444966e133fbaad71
jojo6807 <jyj051811@gmail.com>
2023-09-20 23:02:42 +0900

    행렬의 합

--- 행렬의합.py
+++ 행렬의합.py
@@ -1,6 +1,30 @@
 n , m = map(int, input().split())
-rst = []
+rst1 = []
+rst2 = []
 for i in range(n):
-    a = list(int(input().split()))
-    rst.append(a)
-print(rst)
+    a = list(map(int, input().split()))
+    rst1.append(a)
+print(rst1)
+
+for i in range(m):
+    b = list(map(int, input().split()))
+    rst2.append(b)
+print(rst2)
+
+for i in range(n):
+    rst3 = []
+    for j in range(n):
+        rst3.append(rst1[i][j] + rst2[i][j])
+    print(rst3)
+
+
+# A, B = [], []
+# N , M = map(int, input().split())
+# for i in range(N):
+#     A.append(list(map(int, input().split())))
+# for i in range(M):
+#     B.append(list(map(int, input().split())))
+# for i in range(N):
+#     for j in range(M):
+#         print(A[i][j] + B[i][j], end = ' ')
+#     print()
