00658d5ef815a3d29cc92431cf2d1c0ad36a52e6
jojo6807 <jyj051811@gmail.com>
2023-09-21 23:54:38 +0900

    행렬의 최댓값

--- 행렬 최대값.py
+++ 행렬 최대값.py
@@ -19,7 +19,19 @@ print(max_column)
 print((max_row.index(max(max_row)) + 1), end = ' ')
 print((column_n + 1))
 
-
+# table = [list(map(int, input().split())) for _ in range(9)]
+#
+# max_num = 0
+# max_row, max_col = 0, 0
+# for row in range(9):
+#     for col in range(9):
+#         if max_num <= table[row][col]:
+#             max_row = row + 1
+#             max_col = col + 1
+#             max_num = table[row][col]
+#
+# print(max_num)
+# print(max_row, max_col)
 
 
 
