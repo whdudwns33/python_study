6f67d0322e9f4dc92f50960c1be347232bb631f6
jojo6807 <jyj05181@gmail.com>
2023-10-04 17:48:54 +0900

    세로읽기

---
+++ 세로읽기.py
@@ -0,0 +1,26 @@
+word = []
+for i in range(5):
+    word.append(list(input()))
+a = []
+for i in range(len(word)):
+    a.append(len(word[i]))
+rst = []
+for i in range(max(a)):
+    for j in range(max(a)):
+        if len(word[j]) <= max(a):
+            word[j].append("*")
+        rst.append(word[j][i])
+print(rst)
+
+
+
+# AABCDD
+# afzz
+# 09121
+# a8EWg6
+# P5h3kx
+
+# rst = "".join(list)
+# print(rst.replace("*", ""))
+
+
