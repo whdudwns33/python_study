be9d6af9ccad058fe253c6e44caae6010b7bcf88
jojo6807 <jyj051811@gmail.com>
2023-10-05 22:48:19 +0900

    색종이 넓이

--- 색종이넓이.py
+++ 색종이넓이.py
@@ -1,7 +1,10 @@
 n = int(input())
-
+fir = []
+scd = []
 for i in range(n):
-    fir = list(int(input()))
-    scd = list(int(input()))
+    fir.append(int(input()))
+    scd.append(int(input()))
 
+# 아이디어 
+# 입력 받은 마지막 숫자들에 10을 더해 곱을 한후 여백 부분을 빼면 정답 도출
 
